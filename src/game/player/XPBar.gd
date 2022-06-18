a9c4c992b2689504349ae669043b6b1826d59334
Arthur Medeiros <rthurmedeiros@gmail.com>
2022-06-18 14:59:30 -0300

    Added wave progress indicator

--- src/game/player/XPBar.gd
+++ src/game/player/XPBar.gd
@@ -2,17 +2,28 @@ extends Control
 
 
 export var castle_health_path: NodePath
+export var wave_manager_path: NodePath
 
 onready var xp_label = $Label
 onready var wave_label = $Wave
+onready var wave_progress = $Wave/ProgressBar
 onready var castle_hp_bar = $Castle/HPBar
 onready var castle_health: Health = get_node(castle_health_path)
+onready var wave_manager = get_node(wave_manager_path)
 
 
 func _ready():
 	var _ok
 	_ok = Currency.connect("updated", self, "_on_Currency_updated")
 	_ok = castle_health.connect("damage", self, "_on_Castle_Health_damage")
+	_ok = wave_manager.connect("updated_wave", self, "_on_WaveManager_updated_wave")
+
+
+func _process(_delta):
+	# copy wave progress value
+	var timer: Timer = wave_manager.timer
+	wave_progress.max_value = timer.wait_time
+	wave_progress.value = (timer.wait_time - timer.time_left) / timer.wait_time * 100
 
 
 func _on_Currency_updated():
